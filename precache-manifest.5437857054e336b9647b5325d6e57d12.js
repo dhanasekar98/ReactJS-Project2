7c6498dc870f7f74d63b4c50b0f951a53f7da427
Dhanasekar <m.dhanasekar1998@gmail.com>
2020-07-10 19:16:59 +0530

    Updates

---
+++ precache-manifest.5437857054e336b9647b5325d6e57d12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e6975f66e260efd048e",
+    "url": "/static/css/main.4236e37c.chunk.css"
+  },
+  {
+    "revision": "9e6975f66e260efd048e",
+    "url": "/static/js/main.3ebc5bbf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a57aec3797c65ddd821",
+    "url": "/static/js/2.98c33600.chunk.js"
+  },
+  {
+    "revision": "d0fd43f26cc6d9d2c1a805d08e0c01dd",
+    "url": "/static/media/name.d0fd43f2.png"
+  },
+  {
+    "revision": "750e35162118dbe05e366daa8afddf4b",
+    "url": "/index.html"
+  }
+];
